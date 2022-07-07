ea94891d8f2899c6f84168de4e36de68bbe408ce
thanhbv200585 <87489927+thanhbv200585@users.noreply.github.com>
2022-07-07 21:29:11 +0700

    Add files via upload

--- src/util/AutoPrefix.java
+++ src/util/AutoPrefix.java
@@ -1,18 +1,72 @@
 package util;
 
-public class AutoPrefix {
-    public static String addPrefix(String query) {
-        String prefixes = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                          "PREFIX dct: <http://purl.org/dc/terms/>\n" +
-                          "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-                          "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
-                          "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
-                          "PREFIX dbc: <http://dbpedia.org/resource/Category:>\n" +
-                          "PREFIX dbp: <http://dbpedia.org/property/>\n" +
-                          "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n" +
-                          "PREFIX georss: <http://www.georss.org/georss/>" +
-                          "";
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
-        return prefixes + "\n" + query;
-    }
+public class AutoPrefix {
+	private static HashMap<String, String> prefixes;
+	public static String addPrefixes(String query) {
+		prefixes = prefixes();
+		StringBuffer prefix = new StringBuffer();
+		String temp;
+;		for(String key: prefixes.keySet()) {
+			if(query.contains(key)) {
+				temp = "PREFIX " + key + " " + prefixes.get(key) + "\n";
+				prefix.append(temp);
+			} 
+		}
+		prefix.append(query);
+//		System.out.println(prefix.toString());
+		return prefix.toString();
+	}
+	
+	public static HashMap<String, String> prefixes(){
+		HashMap<String, String> prefixesName = new HashMap<>();
+		String name = "C:\\Users\\DELL\\OneDrive - Hanoi University of Science and Technology\\Desktop\\Study\\20212\\OOP\\OOP-Project\\src\\Code\\Prefixes.rq";
+		try {
+			File file =new File(name);
+			FileReader fr=new FileReader(file);
+			BufferedReader br=new BufferedReader(fr);
+			String line;
+			while((line = br.readLine()) != null) {
+				String[] list = line.split("\\s+");
+				prefixesName.put(list[1], list[2]);
+			}
+			fr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prefixesName;
+	}
+	
+//  FOR TESTING
+//	public static void main (String[] args) {
+//		HashMap<String, String> prefixesName = new HashMap<>();
+//		String name = "C:\\Users\\DELL\\OneDrive - Hanoi University of Science and Technology\\Desktop\\Study\\20212\\OOP\\OOP-Project\\src\\Code\\Prefixes.rq";
+//		try {
+//			File file =new File(name);
+//			FileReader fr=new FileReader(file);
+//			BufferedReader br=new BufferedReader(fr);
+//			String line;
+//			while((line = br.readLine()) != null) {
+//				String[] list = line.split("\\s+");
+//				prefixesName.put(list[1], list[2]);
+//			}
+//			fr.close();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		int i = 1;
+//		for(String key :prefixesName.keySet()) {
+//			System.out.println(i++ + ". " + key + "  " + prefixesName.get(key));
+//		}
+//	}
 }
